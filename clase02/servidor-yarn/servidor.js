983a909a367c3fdf5c0def835a30b5c93ff27be0
Mateo Tibaquira <nestormateogmail.com>
2019-11-25 19:51:55 +0100

    clase02: proyecto con yarn

---
+++ clase02/servidor-yarn/servidor.js
@@ -0,0 +1,13 @@
+// Include it and extract some methods for convenience
+const server = require('server');
+const { get, post } = server.router;
+ 
+// Launch server with options and a couple of routes
+server({ port: 8080 }, [
+  get('/', ctx => 'Hello world'),
+  get('/matheo', () => 'Hola Mateo'),
+  post('/', ctx => {
+    console.log(ctx.data);
+    return 'ok';
+  })
+]);
