d08f48321130efe01a7aee83895f9dd062040c27
Mateo Tibaquira <nestormateogmail.com>
2019-11-27 20:18:17 +0100

    clase03: ng generate lib ui

---
+++ clase03/angular/libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BodyComponent } from './body/body.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [BodyComponent]
+})
+export class UiModule {}
